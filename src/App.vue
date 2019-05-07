07dff5ee088e973216393d5e0f3e4f8d11056414
qinyushuo <576838795@qq.com>
2019-05-07 14:50:05 +0800

    开始

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="app-container">
     <!-- 头部 -->
     <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
 
@@ -8,25 +8,25 @@
 
     <!-- tabbar -->
     <nav class="mui-bar mui-bar-tab">
-      <a class="mui-tab-item mui-active" href="#tabbar">
+      <router-link class="mui-tab-item" to="/home">
         <span class="mui-icon mui-icon-home"></span>
         <span class="mui-tab-label">首页</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-chat">
+      </router-link>
+      <router-link class="mui-tab-item" to="/member">
         <span class="mui-icon mui-icon-contact"></span>
         <span class="mui-tab-label">会员</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-contact">
+      </router-link>
+      <router-link class="mui-tab-item" to="/shopcar">
         <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
           <span class="mui-badge">9</span>
         </span>
         <span class="mui-tab-label">购物车</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-map">
+      </router-link>
+      <router-link class="mui-tab-item" to="/search">
         <span class="mui-icon mui-icon-search"></span>
 
         <span class="mui-tab-label">搜索</span>
-      </a>
+      </router-link>
     </nav>
   </div>
 </template>
@@ -38,4 +38,7 @@ export default {
 </script>
 
 <style>
+.app-container {
+  padding-top: 40px;
+}
 </style>
